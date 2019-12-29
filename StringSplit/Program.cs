0f4ae8b95dd20c0102f3fb0a01aa935bbe70d640
Ediezzle <edmore.mandikiyana@gmail.com>
2019-12-29 20:10:48 +0200

    first and final commit

---
+++ StringSplit/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StringSplit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Taking a string 
+            String str = "Geeks, For Geeks";
+
+            char[] separator = { ',', ' ' };
+            Int32 count = 2;
+
+            // Using the Method 
+            String[] strlist = str.Split(separator,
+                   count, StringSplitOptions.None);
+
+            foreach (String s in strlist)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
